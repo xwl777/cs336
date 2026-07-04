73be7ac3437ded21e1106c5a2f37cb7b1c6d8b92
lwx <lwx@lwxdeMacBook-Air.local>
2026-07-04 22:20:39 +0800

    "optimization version1. update_cnt and count_word are optimized"

--- assignment1-basics/cs336_basics/bpe.py
+++ assignment1-basics/cs336_basics/bpe.py
@@ -39,13 +39,16 @@ def vocab_init(special_tokens:list[str]):
 
 # 计算文本由正则分为多个chunk后各个chunk出现的次数
 def count_word(text:str):
-    cnt = defaultdict(int)
+    word_cnt = defaultdict(int)
+    word_bytes_cnt = defaultdict(int)
     for m in PAT.finditer(text):
         word = m.group()
+        word_cnt[word] += 1
+    for word, cnt in word_cnt.items():
         word_bytes = word2byte(word)
         if len(word_bytes) >= 2: # 这个cnt计数器时用于后续方便计算pair出现的次数，长度为1的word_bytes不产生任何pair，故可以直接忽略
-            cnt[word_bytes] += 1
-    return cnt
+            word_bytes_cnt[word_bytes] += cnt
+    return word_bytes_cnt
 
 # 原始文本可能被special_tokens分为多个文本块，故需要合并得到的计数字典
 def merge_cnt(cnt_lst:list[dict]):
@@ -59,11 +62,13 @@ def merge_cnt(cnt_lst:list[dict]):
 # 返回pair的计数
 def count_pairs(word_cnt:dict):
     pairs_cnt = defaultdict(int)
+    pairs2word = defaultdict(set) # 维护pair到word的反向索引，记录了pair存在于哪些word中
     for word,cnt in word_cnt.items():
         for pair in zip(word[:-1],word[1:]): #将两个可迭代对象打包成元组列表
             pairs_cnt[pair] += cnt
+            pairs2word[pair].add(word)
 
-    return pairs_cnt
+    return pairs_cnt, pairs2word
 
 # 选择计数最大的那个pair
 def max_pair(pairs_cnt:dict):
@@ -84,30 +89,34 @@ def merge(word_bytes:tuple[bytes],merge:tuple[bytes]):
     return tuple(new_word_bytes)
 
 # 合并pair后需要更新pair计数
-def update_cnt(word_cnt:dict, pair_cnt:dict, merge_pair:tuple[bytes]):
-    new_word_cnt = defaultdict(int)
-    new_pair_cnt = defaultdict(int, pair_cnt) # 复制原pair_cnt
+def update_cnt(word_cnt:dict, pair_cnt:dict, pairs2word:dict[tuple[bytes, bytes], set[tuple[bytes, ...]]], merge_pair:tuple[bytes]):
 
-    for word_bytes, cnt in word_cnt.items():
+    affected_words = pairs2word[merge_pair].copy()
+
+    for word_bytes in affected_words:
+        cnt = word_cnt[word_bytes]
         old_pairs = list(zip(word_bytes[:-1], word_bytes[1:]))
-        if merge_pair not in old_pairs: 
-            new_word_cnt[word_bytes] += cnt
-            continue # 该词中没有出现合并的token则无影响
 
-        # 否则将key更新为合并后的word_bytes
+        # 将key更新为合并后的word_bytes，删除原key
         new_word_bytes = merge(word_bytes, merge_pair)
-        new_word_cnt[new_word_bytes] += cnt
+        del word_cnt[word_bytes]
+        word_cnt[new_word_bytes] += cnt
 
-        # 重新计算该词对pair_cnt的影响，将原计数全部删去后重新计数
+        # 对每个old_pair，重新计算该词对pair_cnt的影响，将原计数全部删去后重新计数
         for pair in old_pairs:
-            new_pair_cnt[pair] -= cnt
-            if new_pair_cnt[pair] == 0:
-                del new_pair_cnt[pair] 
-        
-        for pair in zip(new_word_bytes[:-1], new_word_bytes[1:]):
-            new_pair_cnt[pair] += cnt
+            pair_cnt[pair] -= cnt
+            if pair_cnt[pair] == 0:
+                del pair_cnt[pair]
+            pairs2word[pair].discard(word_bytes) # 在反向索引中删除原word
 
-    return new_word_cnt, new_pair_cnt
+        for pair in zip(new_word_bytes[:-1], new_word_bytes[1:]):
+            pair_cnt[pair] += cnt
+            pairs2word[pair].add(new_word_bytes) # 在反向索引中加入新word
+            if not pairs2word[pair]:
+                del pairs2word[pair]
+
+
+    return word_cnt, pair_cnt, pairs2word
 
 
 def train_bpe(input_path:str, vocab_size:int, special_tokens:list[str]):
@@ -117,7 +126,7 @@ def train_bpe(input_path:str, vocab_size:int, special_tokens:list[str]):
     cnt_lst = list(map(count_word, chunks))
     word_cnt = merge_cnt(cnt_lst)
     print("word 计数完成")
-    pairs_cnt = count_pairs(word_cnt)
+    pairs_cnt, pairs2word = count_pairs(word_cnt)
     print("配对计数完成")
     vocab = vocab_init(special_tokens)
     merges = list()
@@ -128,7 +137,7 @@ def train_bpe(input_path:str, vocab_size:int, special_tokens:list[str]):
             print(f"当前进度{len(vocab)}/{vocab_size}")
         merge_pair = max_pair(pairs_cnt)
         merges.append(merge_pair)
-        word_cnt, pairs_cnt = update_cnt(word_cnt, pairs_cnt, merge_pair)
+        word_cnt, pairs_cnt,pairs2word = update_cnt(word_cnt, pairs_cnt, pairs2word, merge_pair)
         vocab[len(vocab)] = merge_pair[0] + merge_pair[1]
 
     return vocab, merges
